9fb99beabb6c765f79047c86b43c242de0656388
1000copy <1000copy@gmail.com>
2018-07-24 17:35:40 +0800

    init

--- test/item.js
+++ test/item.js
@@ -4,7 +4,7 @@ describe('config', function () {
 	 it('csv', async () => {
 			const fs = require('await-fs');
 			try{
-				let input = await fs.readFile('./doc/科目列表.csv','utf8')
+				let input = await fs.readFile('./doc/a.csv','utf8')
 		  	 	const csv = require('async-csv');
 			 	let result2 = await csv.parse(input,{});
 				assert.deepEqual(result2.length,190);
